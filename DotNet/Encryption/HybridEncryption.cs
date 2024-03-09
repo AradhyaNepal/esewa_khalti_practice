a2a9cdfa0dde3a5083ff14c704cddf01915fb80f
Aradhya Nepal <aradhya.1221@gmail.com>
2024-03-09 13:16:56 +0545

    Implemented hybrid encryption

--- DotNet/Encryption/HybridEncryption.cs
+++ DotNet/Encryption/HybridEncryption.cs
@@ -1,25 +1,67 @@
 ﻿using System.CodeDom;
 using System.Security.Cryptography;
 
+///THis entire encrypting need to refactor, lots of coupling happening here and code is not testable.
+///This codes really needs unit tests.
 namespace EsewaPractice.Encryption
 {
+    public class HybridEncryptedResponse {
+        public required string EncryptedData=string.Empty;
+        public required string EncryptedDecryptionKey=string.Empty;
+
+
+    }
     public class HybridEncryption
     {
-        static public string EncryptForMobile(string data) {
-    
-            using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
+
+        static HybridEncryptedResponse HybridEncryptMobile(String data) {
+            var symmetricKey = GenerateAESKey();
+            var encryptedData = EncryptStringAES(data, symmetricKey);
+            var encryptedDecryptionKey = RSAEncryption.EncryptMobile(Convert.ToBase64String(symmetricKey));
+            return new(){
+                EncryptedData=encryptedData,
+                EncryptedDecryptionKey=encryptedDecryptionKey,
+            };
+        }
+
+        // Generate a random AES key
+        static byte[] GenerateAESKey()
+        {
+            using (Aes aesAlg = Aes.Create())
             {
-                aesProvider.KeySize = 256;
-
-                // Generate a new AES key
-                aesProvider.GenerateKey();
-
-                // Get the generated key
-                byte[] aesKey = aesProvider.Key;
-
-            
+                aesAlg.GenerateKey();
+                return aesAlg.Key;
             }
+        }
 
+
+        // Encrypt a string using AES
+        static string EncryptStringAES(string plainText, byte[] key)
+        {
+            byte[] encrypted;
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.Mode = CipherMode.CBC; // Cipher Block Chaining
+                aesAlg.Padding = PaddingMode.PKCS7; // Padding for incomplete blocks
+
+                // Create encryptor
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Encrypt the data
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+            return Convert.ToBase64String(encrypted);
         }
     }
 }
